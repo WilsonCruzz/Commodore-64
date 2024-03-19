0a238ba284d38b6816da40a74e7d1b76df17de06
WilsonCruzz <143129254+WilsonCruzz@users.noreply.github.com>
2024-03-19 11:43:49 -0400

    upload

---
+++ JS/header.js
@@ -0,0 +1,18 @@
+document.querySelector(".header").innerHTML = 
+`<header>
+<h1>Commodore 64</h1>
+<nav> 
+<!-- link to other pages -->
+    <ul class="horizontal-menu">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About Us</a></li>
+        <li><a href="product.html">Product</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+</nav>
+</header>`;
+
+document.querySelector(".footer").innerHTML =
+`    <footer>
+<p>&copy; 2023 Commodore International Limited. All rights reserved.</p>
+</footer>`;
